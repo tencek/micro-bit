b5d57ead8530bdf3908636a134dce9cb8a9ad7c9
Pavel Kučera <teni@zlin6.cz>
2024-11-03 23:07:52 +0100

    Still playing in the sandbox

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
 let x = 0
 let y = 0
 basic.forever(function () {
+    music.setVolume(30)
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
-    if (Math.abs(x) > 32) {
+    if (Math.abs(x) > 50) {
+        music.stopAllSounds()
         basic.showIcon(IconNames.Chessboard)
     } else {
-        if (Math.abs(y) > 32) {
+        if (Math.abs(y) > 50) {
+            music.stopAllSounds()
             basic.showIcon(IconNames.Chessboard)
         } else {
+            music.ringTone(349)
             basic.showIcon(IconNames.SmallDiamond)
         }
     }
